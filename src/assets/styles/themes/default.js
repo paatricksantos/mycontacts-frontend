6c4d3298e30776c706a9aa76bfd42f9aaf402170
Patrick <patrick@pop-os.localdomain>
2021-07-19 10:23:27 -0300

    organizando o app

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      lighter: '#e0e3ff',
+      light: '#6674f4',
+      main: '#5061fc',
+      dark: '#3346f0'
+    },
+    gray: {
+      900: '#222',
+      200: '#bcbcbc'
+    },
+    danger: {
+      light: '#f97171',
+      main: '#fc5050',
+      dark: '#f63131'
+    }
+  }
+};
