30d17a8bf93694f7180f6eb1a8b53e4178f12fd3
Patrick <patrick@pop-os.localdomain>
2021-07-19 12:06:54 -0300

    correcao dos estilos

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
+import ContactsList from '../ContactsList';
 import Header from '../Header';
 import { Container } from './styles';
 
@@ -12,6 +13,7 @@ function App() {
       <GlobalStyles />
       <Container>
         <Header />
+        <ContactsList />
       </Container>
     </ThemeProvider>
   );
