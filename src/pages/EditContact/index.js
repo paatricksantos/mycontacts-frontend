5fda8d8144b203f07d53f49180c60d81f3a0849f
Patrick <patrick@pop-os.localdomain>
2021-07-19 12:25:29 -0300

    criacao da estrutura dos arquivos das paginas

---
+++ src/pages/EditContact/index.js
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function EditContact() {
+  return (
+    <Container>
+      <h1>EditContact</h1>
+    </Container>
+  );
+};
+
+export default EditContact;
---
+++ src/pages/NewContact/index.js
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function NewContact() {
+  return (
+    <Container>
+      <h1>NewContact</h1>
+    </Container>
+  );
+}
+
+export default NewContact;
